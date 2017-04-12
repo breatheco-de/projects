6fb14487866edd7c58e62da2ba0998243c69816d
Alejandro Sanchez <a@stcsolutions.com.ve>
2017-04-12 16:00:02 -0400

    timeline with css

---
+++ p/css/junior/other/timeline/assets/js/script.js
@@ -0,0 +1,65 @@
+// SMOOTH SCROLLING SECTIONS
+$(document).ready(function(){
+
+    // Cache selectors
+    var lastId,
+        topMenu = $("nav.nav__wrapper"),
+        topMenuHeight = topMenu.outerHeight(),
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+          var item = $($(this).attr("href"));
+          if (item.length) { return item; }
+        });
+
+    $('section').each(function(){
+        //$(this).css('background',getRandomColor());
+    });
+
+    // Bind click handler to menu items
+    // so we can get a fancy scroll animation
+    menuItems.click(function(e){
+      var href = $(this).attr("href");
+      console.log(href+", offset: "+$(href).offset().top);
+      var offsetTop = (href === "#") ? 0 : $(href).offset().top+1;
+      $('html, body').stop().animate({ 
+          scrollTop: offsetTop
+      }, 300);
+      e.preventDefault();
+    });
+
+    // Bind to scroll
+    $(window).scroll(function(){
+       // Get container scroll position
+       var fromTop = $(this).scrollTop()+topMenuHeight;
+       
+       // Get id of current scroll item
+       var cur = scrollItems.map(function(){
+         if ($(this).offset().top < fromTop)
+           return this;
+       });
+       // Get the id of the current element
+       cur = cur[cur.length-2];
+       var id = cur && cur.length ? cur[0].id : "";
+       
+       if (lastId !== id) {
+           lastId = id;
+           // Set/remove active class
+           menuItems
+             .parent().removeClass("active")
+             .end().filter("[href='#"+lastId+"']").parent().addClass("active");
+       }                   
+    });
+
+});
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
