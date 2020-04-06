715b043101a7c5adc551ddbbe70251e76b4d8e93
Alejandro Sanchez <aalejo@gmail.com>
2020-04-06 02:57:58 -0400

    project tutorials reloaded

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,5 +1,6 @@
+/* globals Object */
 import React, { useState } from "react";
-import { Filter } from "@breathecode/ui-components";
+import { Filter, CheckBox } from "@breathecode/ui-components";
 import "@breathecode/ui-components/dist/main.css";
 import Layout from "../components/layout"
 import { SmallJumbotron } from "../components/smalljumbo.js";
@@ -8,11 +9,25 @@ import { ProjectCard } from "../components/projectCard.js";
 const Home = ({ pageContext }) => {
     const [ selectedTags, setSelectedTags ] = useState(null);
     const [ difficulty, setDifficulty ] = useState(null);
-    const [ withVideo, setWithVideo ] = useState(false);
+    const [ extraOps, setExtraOps ] = useState([]);
     const [ displayMode, setDisplayMode ] = useState("card");
 
+    const withVideo = extraOps.includes("with-video");
+    const _selectedTech = (!selectedTags || selectedTags.length === 0) ? null : selectedTags;
     const projects = pageContext.projects.filter(p => {
-        if(selectedTags && selectedTags.length>0 && selectedTags.indexOf(p.technology) === -1) return false;
+        
+        if(_selectedTech){
+            if(typeof p.technology === "string"){
+                if(!_selectedTech.includes(p.technology)) return false;
+            }
+            else if(Array.isArray(p.technologies)){
+                if(_selectedTech.filter(t => p.technologies.includes(t)).length === 0) return false;
+            } 
+            else{
+                return false;
+            }
+        } 
+        
         if(difficulty && p.difficulty !== difficulty) return false;
         if(withVideo && !p["video-id"]) return false;
         return true;
@@ -48,7 +63,9 @@ const Home = ({ pageContext }) => {
                                                 label="Difficulty"
                                                 multiselect={false}
                                                 placeholder="Filter by difficulty"
-                                                onChange={(difficulty)=> setDifficulty(difficulty ? difficulty.value : null)}
+                                                onChange={(_difficulty)=> {
+                                                    setDifficulty(typeof(_difficulty[0]) === "object" ? _difficulty[0].value : null)
+                                                }}
                                                 options={pageContext.difficulties ? pageContext.difficulties.map((d)=>{
                                                     return {label: d,value: d}
                                                 }):[{label:" ",value: " "}]}
@@ -57,10 +74,9 @@ const Home = ({ pageContext }) => {
                                     <div className="pl-1">
                                         <Filter
                                                 label="Other filters"
-                                                multiselect={false}
                                                 placeholder="Other filters: "
                                                 direction={'row'}
-                                                onChange={(t)=> setWithVideo(t ? t.value === "with-video" : false)}
+                                                onChange={(opts)=> setExtraOps(opts.map(t => t.value))}
                                                 options={[
                                                     {label: 'Only with video tutorials',value: 'with-video'}
                                                 ]}
